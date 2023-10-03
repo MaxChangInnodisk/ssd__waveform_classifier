fd9d55fa4d89ea0bb683a8054c08506d97aa6341
MaxChangInnodisk <max_chang@innodisk.com>
2023-10-03 13:31:52 +0800

    feat: add write_json function

--- ivit_i/utils.py
+++ ivit_i/utils.py
@@ -44,6 +44,11 @@ def read_json(json_file: str):
         data = json.load(f)
     return data
 
+def write_json(output:dict, json_file:str):
+    with open(json_file, "w") as jsonfile:
+        json.dump(output, jsonfile, indent=4, cls=NpEncoder)
+
+
 def find_folder_with_key(keyword: str, path: str= "./"): 
     input_folders = glob.glob(os.path.join(path, f"*{keyword}*"))
     assert len(input_folders)==1, f"Except only one input folder here but got {len(input_folders)}"
