c4530838ae8b2a71bc415c5569aba0168b790af5
MaxChangInnodisk <max_chang@innodisk.com>
2023-10-05 11:01:30 +0800

    feat: reame and support ini config

---
+++ classifier.py
@@ -0,0 +1,43 @@
+import logging as log
+import subprocess as sp
+from ivit_i.utils import check_dir, read_json, read_ini
+
+def get_exec_cmd(exec_key: str, config: dict) -> str:
+    
+    exec_info = config[exec_key]
+    assert exec_info != None, f"Unexpect exec file key ... ({exec_key})"
+
+    cmd = f"{exec_info['exec']} {exec_info['args']}"
+    log.debug('Get execute command: {}'.format(cmd))
+    return cmd
+
+def check_status(service:str, config:dict):
+    return int(config[service]["enable"])
+
+def run_service(service:str, config:dict):
+    status = check_status(service, config)
+    log.info('--------------------------------------------------------------------------------')
+    log.info(f"SERVICE {service}: {'ON' if status else 'OFF'}.")
+    log.debug('')
+
+    if not status: return None
+
+    try:
+        cmd = get_exec_cmd( 
+            exec_key=service,
+            config=config)
+        return sp.run(cmd, shell=True)
+
+    except KeyboardInterrupt:
+        return None
+
+def main():
+    config = read_ini("config.ini")
+    # Exec
+    run_service(service="aida64", config=config)    
+    run_service(service="ivit", config=config)    
+    
+    key = input("\n\nPress ANY to leave ...")
+
+if __name__ == "__main__":
+    main()
